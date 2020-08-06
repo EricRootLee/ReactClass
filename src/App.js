56684feaf874c2011ce2411f3caf718bd8e21816
EricRootLee <erickiokom@gmail.com>
2020-08-06 08:32:16 +0300

    working with props children

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Person from './Person/Person';
+import person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
-      <Person name="Max" age="29" />
+      <Person name="Max" age="29" > And I play Hocky</Person>
     </div>
   );
-  }
+}
 
 export default App;
