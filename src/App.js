022d2760e5f48f7bd6aa719665d52acd1bd61206
EricRootLee <erickiokom@gmail.com>
2020-08-06 08:53:57 +0300

    manupilating state

--- src/App.js
+++ src/App.js
@@ -13,12 +13,21 @@ class App extends Component {
   }
   switchNameHandler = () => {
     console.log("Clicked")
+    this.setState({
+      person: [
+        { name: 'Muli', age: 26 },
+        { name: 'Manu', age: 26 },
+        { name: 'Steph', age: 26 }
+
+      ]
+    })
   }
   render() {
     return (
       <div className="App">
         <button onClick={this.switchNameHandler} >Switch Name</button>
         <Person name={this.state.person[0].name} age={this.state.person[0].age} > And I play Hocky</Person>
+        <Person name={this.state.person[1].name} age={this.state.person[1].age} > And I play Hocky</Person>
       </div>
     );
   }
