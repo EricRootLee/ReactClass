639d1d16d6cfcf9149133795f8d56acfb9f9e9b1
EricRootLee <erickiokom@gmail.com>
2020-08-09 00:38:04 +0300

    Started On the layout

--- burger-project/src/App.js
+++ burger-project/src/App.js
@@ -1,13 +1,19 @@
-import React from 'react';
+
 import Layout from './components/layout/Layout';
 
+import React, { Component } from 'react'
+import BurgerBuilder from './containers/burgerbuilder/BurgerBuilder';
 
-function App() {
-  return (
-    <div className="App">
-      <Layout />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
+
+      </div>
+    );
+  }
 }
-
 export default App;
